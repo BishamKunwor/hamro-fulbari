9d5eadf4fbf0233c4f1d228fce52e4ee649d3c3a
Bisham Kunwor <novelian.nova@gmail.com>
2023-04-22 22:59:15 +0545

    Added Login feature

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,48 @@
+import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
+
 export default function RegisterAccount() {
+  useEffect(() => {
+    if (Object.keys(router.query).length === 0) {
+      return;
+    }
+    if (router.query.failed) {
+      errorPopup.current?.classList.remove("-top-20");
+      errorPopup.current?.classList.add("top-10");
+      setTimeout(() => {
+        errorPopup.current?.classList.add("-top-20");
+        errorPopup.current?.classList.remove("top-10");
+      }, 1500);
+    }
+  });
+  const router = useRouter();
+  const errorPopup = useRef<HTMLDivElement>(null);
+
   return (
     <section className="max-w-screen-2xl mx-auto py-16 space-y-4">
+      <div
+        ref={errorPopup}
+        className={`absolute left-0 -top-20 w-full transition-all duration-300`}
+      >
+        <div className="alert alert-error shadow-lg max-w-screen-sm mx-auto">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current flex-shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>Error! Incorrect Credentials.</span>
+          </div>
+        </div>
+      </div>
       <h1 className="text-4xl text-center text-primary capitalize">
         Already register ?
       </h1>
@@ -25,7 +67,7 @@ export default function RegisterAccount() {
           <h3 className="font-medium mt-4">
             If you have an account with us, please login here:
           </h3>
-          <form action="" className="w-full">
+          <form action="/successful-login" className="w-full">
             <div className="form-control w-full">
               <label className="label">
                 <span className="label-text font-bold text-black/60">
@@ -33,6 +75,7 @@ export default function RegisterAccount() {
                 </span>
               </label>
               <input
+                name="email"
                 type="text"
                 placeholder="Type here"
                 className="input input-bordered w-full"
@@ -45,7 +88,8 @@ export default function RegisterAccount() {
                 </span>
               </label>
               <input
-                type="text"
+                name="password"
+                type="password"
                 placeholder="Type here"
                 className="input input-bordered w-full"
               />
---
+++ pages/successful-login.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import store from "store";
+
+export default function SuccessfulLogin() {
+  useEffect(() => {
+    if (Object.keys(router.query).length === 0) {
+      return;
+    }
+    for (let data of store.get("userCredentials")) {
+      if (
+        data.email === router.query.email &&
+        data.password === router.query.password
+      ) {
+        store.set("activeUser", data);
+        router.push("/");
+      } else {
+        router.push("/login?failed=true");
+      }
+    }
+  });
+  const router = useRouter();
+  return <></>;
+}
