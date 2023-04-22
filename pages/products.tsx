bfa95b53203d3d494abeca32a50b28748a670bb1
Bisham Kunwor <novelian.nova@gmail.com>
2023-04-23 04:46:39 +0545

    revolved add to cart not working in products page

--- pages/products.tsx
+++ pages/products.tsx
@@ -40,69 +40,105 @@ export default function Products() {
   };
 
   return (
-    <section className="flex max-w-screen-2xl mx-auto gap-10 mb-16">
-      <div className="border px-12 border-primary bg-secondary max-w-xs">
-        <h2 className="text-2xl text-primary my-2">Products</h2>
-        <div className="mt-8">
-          <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">New Arrivals</div>
-          <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">Nursery plants</div>
-          <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">Garden Essentials</div>
-          <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">Indoor Plants</div>
-          <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">Outdoor Plants</div>
-          <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">Chemical</div>
-          <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">Trees</div>
+    <>
+      <section className="flex max-w-screen-2xl mx-auto gap-10 mb-16">
+        <div className="border px-12 border-primary bg-secondary max-w-xs">
+          <h2 className="text-2xl text-primary my-2">Products</h2>
+          <div className="mt-8">
+            <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">
+              New Arrivals
+            </div>
+            <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">
+              Nursery plants
+            </div>
+            <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">
+              Garden Essentials
+            </div>
+            <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">
+              Indoor Plants
+            </div>
+            <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">
+              Outdoor Plants
+            </div>
+            <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">
+              Chemical
+            </div>
+            <div className="text-xl whitespace-nowrap cursor-pointer mt-2 hover:text-primary transition-all duration-300">
+              Trees
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="w-full">
-        <h2 className="text-3xl text-primary my-2">Products</h2>
-        <main className="flex gap-6 flex-wrap">
-          {plantsData.map((data, index) => {
-            return (
-              <div
-                key={index}
-                className="card gap-6 w-60 py-4 px-8 items-start border border-primary bg-base-100 hover:shadow-xl rounded-none bg-transparent transition-all duration-300 cursor-pointer"
-              >
-                <figure>
-                  <img
-                    className="h-44 border-primary"
-                    src={data.imgUrl}
-                    alt="Shoes"
-                  />
-                </figure>
-                <div className="card-body p-0">
-                  <h2 className="card-title">{data.name}</h2>
-                  <p>Rs. {data.price}</p>
-                  <div className="card-actions justify-start">
-                    {isLoggedIn ? (
-                      <button
-                        onClick={() => {
-                          handleAdd(data);
-                          updateCartAmount();
-                          message.success("Successfully Added Element.");
-                        }}
-                        className="btn btn-primary w-40 bg-primary border-primary hover:bg-primary/80 hover:border-primary/80"
-                      >
-                        Add to Cart
-                      </button>
-                    ) : (
-                      <label
-                        htmlFor="my-modal-4"
-                        className="btn w-40 bg-primary border-primary hover:bg-primary/80 hover:border-primary/80"
-                      >
-                        Add to Cart
-                      </label>
-                    )}
-                    <a href={`/details?id=${data.id}`} className="btn w-40">
-                      View Details
-                    </a>
+        <div className="w-full">
+          <h2 className="text-3xl text-primary my-2">Products</h2>
+          <main className="flex gap-6 flex-wrap">
+            {plantsData.map((data, index) => {
+              return (
+                <div
+                  key={index}
+                  className="card gap-6 w-60 py-4 px-8 items-start border border-primary bg-base-100 hover:shadow-xl rounded-none bg-transparent transition-all duration-300 cursor-pointer"
+                >
+                  <figure>
+                    <img
+                      className="h-44 border-primary"
+                      src={data.imgUrl}
+                      alt="Shoes"
+                    />
+                  </figure>
+                  <div className="card-body p-0">
+                    <h2 className="card-title">{data.name}</h2>
+                    <p>Rs. {data.price}</p>
+                    <div className="card-actions justify-start">
+                      {isLoggedIn ? (
+                        <button
+                          onClick={() => {
+                            handleAdd(data);
+                            updateCartAmount();
+                            message.success("Successfully Added Element.");
+                          }}
+                          className="btn btn-primary w-40 bg-primary border-primary hover:bg-primary/80 hover:border-primary/80"
+                        >
+                          Add to Cart
+                        </button>
+                      ) : (
+                        <label
+                          htmlFor="my-modal-4"
+                          className="btn w-40 bg-primary border-primary hover:bg-primary/80 hover:border-primary/80"
+                        >
+                          Add to Cart
+                        </label>
+                      )}
+                      <a href={`/details?id=${data.id}`} className="btn w-40">
+                        View Details
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </main>
-      </div>
-    </section>
+              );
+            })}
+          </main>
+        </div>
+      </section>
+      <section id="login-register-prompt-modal">
+        <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+        <label htmlFor="my-modal-4" className="modal cursor-pointer">
+          <label className="modal-box relative" htmlFor="">
+            <h3 className="text-lg font-bold">Please Login to Continue.</h3>
+            <div className="flex justify-between">
+              <a href="/register">
+                <button className="btn inline-block btn-primary w-40 bg-primary border-primary hover:bg-primary/80 hover:border-primary/80 mt-8">
+                  Register
+                </button>
+              </a>
+              <a href="/login">
+                <button className="btn inline-block btn-primary w-40 bg-primary border-primary hover:bg-primary/80 hover:border-primary/80 mt-8">
+                  Login
+                </button>
+              </a>
+            </div>
+          </label>
+        </label>
+      </section>
+    </>
   );
 }
 
